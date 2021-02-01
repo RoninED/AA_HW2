4525e7d4c1d5a2cd5d2c3456701eeb5391fc5479
RoED <nameeduard@yandex.ru>
2021-02-01 19:12:09 +0300

    dsadas

---
+++ app/src/main/java/com/example/hw2/FragmentMoviesDetails.kt
@@ -0,0 +1,19 @@
+package com.example.hw2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails() : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+        return view
+    }
+}
